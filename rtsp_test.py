3a4277a7d2a6a58231d50580ce33bb554d056a09
chobitd tai <chobits@itri.org.tw>
2022-07-22 09:47:24 +0800

    gstreamer no latency

--- rtsp_test.py
+++ rtsp_test.py
@@ -1,6 +1,12 @@
-import cv2
+import cv2, os
 
-vidCap = cv2.VideoCapture('rtsp://192.168.0.34:8554/unicast')
+#os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "flags;no_delay"
+print(cv2.getBuildInformation())
+#vidCap = cv2.VideoCapture('rtsp://192.168.0.34:8554/unicast', cv2.CAP_FFMPEG)
+vidCap = cv2.VideoCapture('rtspsrc location=rtsp://192.168.0.34:8554/unicast latency=0 ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
+#print(vidCap.isOpened())
+#print(vidCap.get(cv2.CAP_PROP_BUFFERSIZE))
+#vidCap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 cv2.namedWindow('image_display', cv2.WINDOW_AUTOSIZE)
 
 while True:
@@ -8,10 +14,11 @@ while True:
 
     if ret:
         cv2.imshow('image_display', image)
-        key = cv2.waitKey(10)
+        key = cv2.waitKey(1)
         if key == 113: #q
             break
     else:
+        print('nothing')
         break
 
 vidCap.release()
