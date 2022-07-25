85c91a225d6b32bf55d9b5137d007f60a41507ad
chobitd tai <chobits@itri.org.tw>
2022-07-25 16:09:47 +0800

    fix heading

--- detect_aruco_marker.py
+++ detect_aruco_marker.py
@@ -1,8 +1,21 @@
 # import the OpenCV library for computer vision
-import cv2, time
+import cv2, time, math
 import numpy as np
 from pymavlink import mavutil
 from pyquaternion import Quaternion
+
+def rotationMatrixToEulerAngles(R) :
+    sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
+    singular = sy < 1e-6
+    if  not singular :
+        x = math.atan2(R[2,1] , R[2,2])
+        y = math.atan2(-R[2,0], sy)
+        z = math.atan2(R[1,0], R[0,0])
+    else :
+        x = math.atan2(-R[1,2], R[1,1])
+        y = math.atan2(-R[2,0], sy)
+        z = 0
+    return np.array([x, y, z])
 
 cv2.namedWindow('image_display', cv2.WINDOW_AUTOSIZE)
 
@@ -13,7 +26,7 @@ dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
 # Initialize the detector parameters using default values
 parameters = cv2.aruco.DetectorParameters_create()
 
-drone_ip='192.168.0.215'
+drone_ip='192.168.0.34'
 # initialize the webcam as "camera" object
 #camera = cv2.VideoCapture("rtsp://192.168.0.34:8554/unicast")
 camera = cv2.VideoCapture('rtspsrc location=rtsp://'+drone_ip+':8554/unicast latency=0 ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
@@ -22,6 +35,13 @@ camera = cv2.VideoCapture('rtspsrc location=rtsp://'+drone_ip+':8554/unicast lat
 
 master = mavutil.mavlink_connection(device='udpout:'+drone_ip+':17500', source_system=255)
 last_send_ts = time.time()
+got_attitude = False
+
+#camMat = np.array([[978.05583, 0, 644.3227087], [0, 980.40099676, 377.5166175], [0, 0, 1]])
+#distCoeffs = np.array([0.1542403792, -0.207237266, 0.00062078491848, 0.00089082489, -0.2166006565])
+camMat = np.array([[9.7805583154190560e+02, 0, 6.4432270873931213e+02], [0, 9.8040099676993566e+02, 3.7751661754419627e+02], [0, 0, 1]])
+distCoeffs = np.array([1.5424037927595446e-01, -2.0723726675535267e-01, 6.2078491848616114e-04, 8.9082489283745796e-04, -2.1660065658513647e-01])
+rot_y = np.array([[0,0,-1],[0,1,0],[1,0,0]]) #camera x axis to right, drone x axis to forward
 
 # loop that runs the program forever
 # at least until the "q" key is pressed
@@ -43,16 +63,21 @@ while True:
         if markerIds is not None:
             now_ts = time.time()
             if now_ts - last_send_ts > 0.1:
-                last_send_ts = now_ts
-
-                rvecs , tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(markerCorners, 0.2, np.array([[978.05583, 0, 644.3227087], [0, 980.40099676, 377.5166175], [0, 0, 1]]), np.array([0.1542403792, -0.207237266, 0.00062078491848, 0.00089082489, -0.2166006565]))
+                rvecs , tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(markerCorners, 0.2, camMat , distCoeffs)
+                #cv2.drawFrameAxes(img, camMat, distCoeffs, rvecs, tvecs, 0.5)
 
                 r_mtx = cv2.Rodrigues(rvecs[0][0])
+                #print(rotationMatrixToEulerAngles(r_mtx[0]))
+                #print(r_mtx[0])
                 r_mtx = r_mtx[0].transpose()
                 pos = np.matmul(r_mtx, np.negative(tvecs[0][0]))
-                r_quat = Quaternion(matrix=r_mtx)
-                master.mav.att_pos_mocap_send(int(now_ts*1000000), (r_quat.w, r_quat.x, r_quat.z, -r_quat.y), pos[0], pos[2], -pos[1])
+                r_quat = Quaternion(matrix=np.matmul(rot_y, r_mtx))
+                master.mav.att_pos_mocap_send(int(now_ts*1000000), (r_quat.w, r_quat.x, -r_quat.z, r_quat.y), pos[0], -pos[2], pos[1])
+                last_send_ts = now_ts
                 #print(markerIds[0], pos)
+            #else:
+            #    rvecs , tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(markerCorners, 0.2, camMat , distCoeffs)
+            #    cv2.drawFrameAxes(img, camMat, distCoeffs, rvecs, tvecs, 0.5)
 
             # for every tag in the array of detected tags...
             #for i in range(len(markerIds)):
@@ -82,9 +107,12 @@ while True:
         cv2.imshow('image_display', img)
 
     # handler to press the "q" key to exit the program
-    if cv2.waitKey(10) & 0xff == 113 :
+    usr_key = cv2.waitKey(1)
+    if  usr_key & 0xff == 113:
         print("bye")
         break
+    elif usr_key & 0xff == 114:
+        master.mav.command_long_send(0, 0, 246, 0, 1, 0, 0, 0, 0, 0, 0) # reboot
     
     msg = master.recv_msg()
     if msg is not None:
@@ -94,6 +122,12 @@ while True:
         elif msg_type == "TIMESYNC":
             if msg.tc1 == 0: # ardupilot send a timesync message every 10 seconds
                 master.mav.system_time_send(int(time.time() * 1000000), 0) # ardupilot ignore time_boot_ms
+        elif msg_type == "HEARTBEAT":
+            if not got_attitude:
+                master.mav.command_long_send(0, 0, 511, 0, 30, 2000000, 0, 0, 0, 0, 0)
+        elif msg_type == "ATTITUDE":
+            #print('heading', msg.yaw*180/math.pi)
+            pass
 
 # When everything done, release the capture
 camera.release()
