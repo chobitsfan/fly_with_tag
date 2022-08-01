56375be0defbf45f8da66703d7b5b9a2bba6dfeb
chobitd tai <chobits@itri.org.tw>
2022-08-01 16:46:56 +0800

    plnd ok

--- detect_aruco_marker.py
+++ detect_aruco_marker.py
@@ -1,4 +1,6 @@
 # import the OpenCV library for computer vision
+import os
+os.environ["MAVLINK20"] = "1"
 import cv2, time, math
 import numpy as np
 from pymavlink import mavutil
@@ -38,8 +40,8 @@ camera = cv2.VideoCapture('rtspsrc location=rtsp://'+drone_ip+':8554/unicast lat
 rec_vid = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (1280,720))
 
 master = mavutil.mavlink_connection(device='udpout:'+drone_ip+':17500', source_system=255)
-last_send_ts = time.time()
-got_attitude = False
+last_hb_send_ts = time.time()
+#got_attitude = False
 
 #camMat = np.array([[978.05583, 0, 644.3227087], [0, 980.40099676, 377.5166175], [0, 0, 1]])
 #distCoeffs = np.array([0.1542403792, -0.207237266, 0.00062078491848, 0.00089082489, -0.2166006565])
@@ -57,7 +59,7 @@ rot_y = np.array([[0,0,1],[0,1,0],[-1,0,0]]) #camera x axis to right, drone x ax
 # loop that runs the program forever
 # at least until the "q" key is pressed
 while True:
-
+    now_ts = time.time()
     # creates an "img" var that takes in a camera frame
     ret, img = camera.read()
     if ret:
@@ -73,12 +75,16 @@ while True:
             #imgpts, jac = cv.projectPoints(axis, cv2.Rodrigues(tag.pose_R), tag.pose_t, camMat, distCoeffs)
             cv2.drawFrameAxes(img, camMat, distCoeffs, cv2.Rodrigues(tag.pose_R)[0], tag.pose_t, 0.5)
 
-            heading_r_mtx = np.matmul(rot_y, tag.pose_R)
-            heading_r_mtx = heading_r_mtx.transpose()
-            r_mtx = tag.pose_R.transpose()
-            pos = np.matmul(r_mtx, np.negative(tag.pose_t))
-            r_quat = Quaternion(matrix=heading_r_mtx)
-            master.mav.att_pos_mocap_send(int(time.time()*1000000), (r_quat.w, r_quat.x, -r_quat.z, r_quat.y), pos[0], -pos[2], pos[1])
+            # heading_r_mtx = np.matmul(rot_y, tag.pose_R)
+            # heading_r_mtx = heading_r_mtx.transpose()
+            # r_mtx = tag.pose_R.transpose()
+            # pos = np.matmul(r_mtx, np.negative(tag.pose_t))
+            # r_quat = Quaternion(matrix=heading_r_mtx)
+            # master.mav.att_pos_mocap_send(int(time.time()*1000000), (r_quat.w, r_quat.x, -r_quat.z, r_quat.y), pos[0], -pos[2], pos[1])
+
+            #print(np.linalg.norm(tag.pose_t))
+            if master.mavlink20():
+                master.mav.landing_target_send(int(now_ts*1000000), 0, 0, 0, 0, np.linalg.norm(tag.pose_t), 0, 0, -tag.pose_t[1], tag.pose_t[0], tag.pose_t[2], (1, 0, 0, 0), 0, 1, )
 
         # detect aruco tags within the frame
         #markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(img, dictionary, parameters=parameters)
@@ -152,7 +158,13 @@ while True:
         print("bye")
         break
     elif usr_key & 0xff == 114:
+        print("reboot fc")
         master.mav.command_long_send(0, 0, 246, 0, 1, 0, 0, 0, 0, 0, 0) # reboot
+
+    if now_ts - last_hb_send_ts > 2:
+        last_hb_send_ts = now_ts
+        #print("send heartbeat")
+        master.mav.heartbeat_send(6, 8, 0, 0, 0)
     
     msg = master.recv_msg()
     #msg = None
@@ -162,8 +174,9 @@ while True:
             print ("[", msg.get_srcSystem(),"]", msg.text)
         elif msg_type == "TIMESYNC":
             if msg.tc1 == 0: # ardupilot send a timesync message every 10 seconds
-                master.mav.system_time_send(int(time.time() * 1000000), 0) # ardupilot ignore time_boot_ms
+                master.mav.system_time_send(int(now_ts * 1000000), 0) # ardupilot ignore time_boot_ms
         #elif msg_type == "HEARTBEAT":
+            #print(master.mavlink20())
             #if not got_attitude:
                 #master.mav.command_long_send(0, 0, 511, 0, 30, 2000000, 0, 0, 0, 0, 0)
         #elif msg_type == "ATTITUDE":
